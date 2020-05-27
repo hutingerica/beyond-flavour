297d7b8284de1dff18d19663a49f7fff03201466
stackbit-projects <projects@stackbit.com>
2020-05-27 14:39:00 +0000

    Update configuration

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ The content of this site is managed by Forestry. Visit [https://forestry.io](htt
 
 1. run the following command to fetch additional site contents from Stackbit if needed:
 
-        npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__
+        npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5ece7b6f18d252001249683d
 
 1. Starts a development server
 
--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,13 +4,13 @@ set -e
 set -o pipefail
 set -v
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/pull > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5ece7b6f18d252001249683d/webhook/build/pull > /dev/null
 if [[ -z "${STACKBIT_API_KEY}" ]]; then
     echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
 else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__ 
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5ece7b6f18d252001249683d 
 fi
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/ssgbuild > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5ece7b6f18d252001249683d/webhook/build/ssgbuild > /dev/null
 gatsby build
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/publish > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5ece7b6f18d252001249683d/webhook/build/publish > /dev/null
